4eec56600ba2d463b7232ec7e1d639dfc2da412e
niko <Gaya9578>
2021-10-27 15:22:07 +0200

    createBoard and menu of difficulty

--- src/boardSetting.java
+++ src/boardSetting.java
@@ -1,5 +1,34 @@
 public class boardSetting {
     public static void createBoard() {
-        System.out.println("This the board");
+        System.out.println("Select level");
+        int height=0;
+        int timer = 0;
+        String menu= """
+                1 : Easy
+                2 : Medium
+                3 : Hard
+                """;
+     //   int level=get_usr_input(menu,'1','2','3');
+        switch (get_usr_input(menu,'1','2','3')) {
+            case 1:
+                height = 5;
+                timer = 4000;
+            case 2:
+                height = 7;
+                timer = 3500;
+            case 3:
+                height = 10;
+                timer = 3000;
+        }
+        String [][]board = new board [height][height];
+
+
+
+
     }
+
+
 }
+
+
+
