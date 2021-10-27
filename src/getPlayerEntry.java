920d82e0ee815f33c864159a3924b3e25c36e401
Laurence Bellebouche <luby.laurence@gmail.com>
2021-10-27 15:00:57 +0200

    Get_usr_choice method created

--- src/getPlayerEntry.java
+++ src/getPlayerEntry.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class getPlayerEntry {
-    static String PlayerEntry(){
-        String entry = "";
+    static String getEntry(){
+        String entry;
         Scanner scanner = new Scanner(System.in);
         do {
             System.out.println("Where is the beaver, enter number coordinates as : x,y");
@@ -17,5 +17,22 @@ public class getPlayerEntry {
          */
         String regex = "\\d,\\d";
         return regex.matches(p_s_entry);
+    }
+
+    static String get_usr_choice(String menuList, String A, String B, String C) {
+        /*
+        Displays possibles choices and make sure that it is a valid choice.
+         */
+        Scanner scanner = new Scanner(System.in);
+        String choice;
+        do {
+            System.out.println(menuList);
+            System.out.println("Type your choice :");
+            choice = scanner.nextLine();
+            if (!(choice.equals(A) || choice.equals(B) || choice.equals(C)))
+                System.out.println("Please enter a valid choice.\n");
+        }
+        while (!(choice.equals(A) || choice.equals(B) || choice.equals(C)));
+        return choice;
     }
 }
--- src/mainBeaversKillers.java
+++ src/mainBeaversKillers.java
@@ -5,36 +5,19 @@ public class mainBeaversKillers {
         // Welcome message
         System.out.println("Welcome !");
         // Listen to user answer
-        String choice = get_user_choice();
+        String menu = """
+                You can choose between these options :
+                ***** Exercise 1 --> type P
+                ***** Exercise 2 --> type H
+                ***** Quit app   --> type Q""";
+        String choice = getPlayerEntry.get_usr_choice(menu, "P", "H", "Q");
         while (!choice.equals("Q")) {
             switch (choice) {
-                case "J" -> System.out.println("Exercise J");
+                case "P" -> System.out.println("Let's play!");
                 case "H" -> HighScore.toto();
             }
-            choice = get_user_choice();
+            choice = getPlayerEntry.get_usr_choice(menu, "P", "H", "Q");
         }
         System.out.println("Bye bye! See ya!!");
-
-    }
-
-    static String get_user_choice() {
-        /*
-        Displays possibles choices and make sure that it is a valid choice.
-         */
-        Scanner scanner = new Scanner(System.in);
-        String choice;
-        do {
-            System.out.println("""
-                    You can choose between these options :
-                    ***** Exercise 1 --> type J
-                    ***** Exercise 2 --> type H
-                    ***** Quit app   --> type Q""");
-            System.out.println("Choose which exercise you want to do :");
-            choice = scanner.nextLine();
-            if (!(choice.equals("J") || choice.equals("H") || choice.equals("Q")))
-                System.out.println("Please enter a valid choice.\n");
-        }
-        while (!(choice.equals("J") || choice.equals("H") || choice.equals("Q")));
-        return choice;
     }
 }
