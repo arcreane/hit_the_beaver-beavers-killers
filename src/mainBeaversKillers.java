71576e7a1d442583be5a7bf939c0a7e912b82e06
Laurence Bellebouche <luby.laurence@gmail.com>
2021-10-27 10:26:16 +0200

    Fix Case "Q" in Welcome menu

--- src/mainBeaversKillers.java
+++ src/mainBeaversKillers.java
@@ -10,25 +10,24 @@ public class mainBeaversKillers {
             switch (choice) {
                 case "J" -> System.out.println("Exercise 1");
                 case "H" -> System.out.println("Exercise 2");
-                case "Q" -> System.out.println("Bye bye! See ya!!");
             }
             choice = get_user_choice();
         }
-
+        System.out.println("Bye bye! See ya!!");
     }
 
-    static String get_user_choice(){
+    static String get_user_choice() {
         /*
         Displays possibles choices and make sure that it is a valid choice.
          */
         Scanner scanner = new Scanner(System.in);
         String choice;
-        do{
+        do {
             System.out.println("""
-                You can choose between these options :
-                ***** Exercise 1 --> type J
-                ***** Exercise 2 --> type H
-                ***** Quit app   --> type Q""");
+                    You can choose between these options :
+                    ***** Exercise 1 --> type J
+                    ***** Exercise 2 --> type H
+                    ***** Quit app   --> type Q""");
             System.out.println("Choose which exercise you want to do :");
             choice = scanner.nextLine();
             if (!(choice.equals("J") || choice.equals("H") || choice.equals("Q")))
