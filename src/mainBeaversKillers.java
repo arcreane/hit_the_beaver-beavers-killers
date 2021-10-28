a0a88eeb3a0e2c3bbddfa3359305d5a620778c0a
Laurence Bellebouche <luby.laurence@gmail.com>
2021-10-28 14:53:14 +0200

    Main tests

--- src/mainBeaversKillers.java
+++ src/mainBeaversKillers.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class mainBeaversKillers {
     public static void main(String[] args) {
@@ -7,13 +6,13 @@ public class mainBeaversKillers {
         // Listen to user answer
         String menu = """
                 You can choose between these options :
-                ***** Exercise 1 --> type P
-                ***** Exercise 2 --> type H
-                ***** Quit app   --> type Q""";
+                ***** Play             --> type P
+                ***** View High Scores --> type H
+                ***** Quit game        --> type Q""";
         String choice = getPlayerEntry.get_usr_choice(menu, "P", "H", "Q");
         while (!choice.equals("Q")) {
             switch (choice) {
-                case "P" -> System.out.println("Let's play!");
+                case "P" -> boardSetting.createBoard();
                 case "H" -> HighScore.toto();
             }
             choice = getPlayerEntry.get_usr_choice(menu, "P", "H", "Q");
