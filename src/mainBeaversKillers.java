f8582a761b13d731399a4eb03e235baae418b4a4
Cyrille <cyrille.george@edu.esiee-it.com>
2021-10-28 11:14:03 +0200

    draft time controller continue

--- src/mainBeaversKillers.java
+++ src/mainBeaversKillers.java
@@ -10,37 +10,16 @@ public class mainBeaversKillers {
                 ***** Exercise 1 --> type P
                 ***** Exercise 2 --> type H
                 ***** Quit app   --> type Q""";
-        String choice = getPlayerEntry.get_usr_choice(menu, "P", "H", "Q");
+        String choice = "";
         while (!choice.equals("Q")) {
             switch (choice) {
-                case "J" -> boardSetting.createBoard();
+                case "P" -> TimerModule.Timer();
                 case "H" -> HighScore.toto();
-                case "t" -> TimerModule.Timer(2000);
+//                case "t" -> TimerModule.Timer();
             }
-            choice = get_user_choice();
+            choice = getPlayerEntry.get_usr_choice(menu, "P", "H", "Q");
         }
         System.out.println("Bye bye! See ya!!");
-
     }
 
-    static String get_user_choice() {
-        /*
-        Displays possibles choices and make sure that it is a valid choice.
-         */
-        Scanner scanner = new Scanner(System.in);
-        String choice;
-        do {
-            System.out.println("""
-                    You can choose between these options :
-                    ***** Exercise 1 --> type J
-                    ***** Exercise 2 --> type H
-                    ***** Quit app   --> type Q""");
-            System.out.println("Choose which exercise you want to do :");
-            choice = scanner.nextLine();
-            if (!(choice.equals("J") || choice.equals("H") || choice.equals("Q")))
-                System.out.println("Please enter a valid choice.\n");
-        }
-        while (!(choice.equals("J") || choice.equals("H") || choice.equals("Q")));
-        return choice;
-    }
 }
