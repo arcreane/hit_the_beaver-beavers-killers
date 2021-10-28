8140350aa12b5f43dd7109d49aa0d36af6ee34a6
niko <Gaya9578>
2021-10-28 10:38:05 +0200

    create

--- src/boardSetting.java
+++ src/boardSetting.java
@@ -1,27 +1,51 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class boardSetting {
     public static void createBoard() {
         System.out.println("Select level");
-        int height=0;
+        int height = 0;
         int timer = 0;
-        String menu= """
+        String menu = """
                 1 : Easy
                 2 : Medium
                 3 : Hard
                 """;
-     //   int level=get_usr_input(menu,'1','2','3');
-        switch (get_usr_input(menu,'1','2','3')) {
-            case 1:
+        String level = getPlayerEntry.get_usr_choice(menu, "1", "2", "3");
+        switch (level) {
+            case "1":
                 height = 5;
                 timer = 4000;
-            case 2:
+                break;
+            case "2":
                 height = 7;
                 timer = 3500;
-            case 3:
+                break;
+            case "3":
                 height = 10;
                 timer = 3000;
         }
-        String [][]board = new board [height][height];
 
+
+        StringBuilder build = new StringBuilder();
+        String[][] board = new String[height][height];
+
+        for (int i = 0; i < board.length + 1; i++) {
+            build.append(1 + "   ");
+        }
+        build.append("\n");
+        for (int i = 0; i < board.length + 1; i++) {
+
+            build.append("___");
+        }
+        build.append("\n");
+        for (int i = 0; i < height; i++) {
+            build.append(Stream.of(board[i])
+                    .collect(Collectors.joining()));
+            System.out.println(Arrays.toString(board[i]));
+        }
 
 
 
