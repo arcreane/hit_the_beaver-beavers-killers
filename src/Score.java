f8028177b26345546bda2bbc2be8db308332593a
Laurence Bellebouche <luby.laurence@gmail.com>
2021-10-29 12:17:15 +0200

    Fix High Scores printing and saving High Scores.

--- src/Score.java
+++ src/Score.java
@@ -1,11 +1,13 @@
 import java.io.*;
 
 public class Score {
+    /**
+     * Searches for fileName.txt file and reads it line by line to
+     * @param fileName
+     * @return an array of Strings.
+     */
     public static String[][] getSavedScore(String fileName){
-        /**
-         * Searches for fileName.txt file and converts it into an array of Strings.
-         */
-        String[][] inputFile = new String[5][2];
+        String[][] inputFile = new String[15][2];
 
         // Create a file object
         File file = new File(fileName);
@@ -18,10 +20,11 @@ public class Score {
             BufferedReader bufferedReader = new BufferedReader(reader);
             String line;
             int row = 0;
-            while ((line = bufferedReader.readLine()) != null && row<2) {
+            while ((line = bufferedReader.readLine()) != null && row<15) {
                 String[] data = line.split(" ");
                 for(int i = 0; i<2; i++)
-                    inputFile[row++][i] = data[i];
+                    inputFile[row][i] = data[i];
+                row++;
             }
             reader.close();
 
@@ -31,19 +34,21 @@ public class Score {
         return inputFile;
     }
 
+    /**
+     * Gets FileName.txt and rewrites it with the new high scores.
+     * @param fileName
+     * @param outputFile
+     */
     public static void overwriteScores (String fileName, String[][] outputFile){
-        /**
-         * Gets FileName.txt and rewrites it with the new high scores.
-         */
         File file = new File(fileName);
         String absolute = file.getAbsolutePath();
         try {
-            FileWriter fooWriter = new FileWriter(absolute, false); // true to append
+            FileWriter writer = new FileWriter(absolute, false); // true to append
             // false to overwrite.
             for(String[] row:outputFile){
-                fooWriter.write(row[0]+" "+row[1] +"\n");
+                writer.write(row[0]+" "+row[1] +"\n");
             }
-            fooWriter.close();
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
