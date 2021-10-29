10a24914fc3e41f4212f169f2aa17be7456f8336
Cyrille <cyrille.george@edu.esiee-it.com>
2021-10-29 12:12:50 +0200

    Added game log into Prefs and added time spent to answer

--- src/mainBeaversKillers.java
+++ src/mainBeaversKillers.java
@@ -1,11 +1,20 @@
 import java.io.IOException;
 
 public class mainBeaversKillers {
+    /**
+     * Program Main: start the game and initialise parameters
+     *
+     * @param args not used
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        // Welcome message
+        //  initialisation
         HighScore.init_HighScores();
         getPlayerEntry.board_Mode = 1;
         boardSetting.fillUp();
+        GamePlay.init_Log();
+
+        // Welcome message
         System.out.println("Welcome !");
         // Listen to user answer
         String menu = """
@@ -25,5 +34,4 @@ public class mainBeaversKillers {
         }
         System.out.println("Bye bye! See ya!!");
     }
-
 }
